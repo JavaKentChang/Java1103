2b95a3f1c72db16c6867141519923c0ab39f0227
MB-study <MB-study@192.168.1.109>
2020-11-03 21:15:46 +0800

    Java-Calc3

--- src/main/java/com/ocp/BMI.java
+++ src/main/java/com/ocp/BMI.java
@@ -10,12 +10,15 @@ public class BMI {
         return bmi;
     }
     double calc(int sex){
-        double bmi=w /Math.pow(h/100, 2);
+        //double bmi=w /Math.pow(h/100, 2);
+        double bmi = calc();
         switch(sex){
             case 1:
                 bmi*=1.1;
+                break;
             case 2:
                 bmi*=0.9;
+                break;
             default:
                 bmi=0;
         }
--- src/main/java/com/ocp/Calc.java
+++ src/main/java/com/ocp/Calc.java
@@ -12,6 +12,18 @@ public class Calc {
         //第二種有回傳值的方法;
         int sum2 = a1.calcNotPrint();//回傳一個int值後，接起來。
         System.out.println(sum2);//印出那個回傳值
+        //---------------------------------------
+        BMI bmi = new BMI();//問老師這行的意思為何？
+        bmi.h = 170.0;
+        bmi.w = 60.0;
+        double b1 = bmi.calc();
+        double b2 = bmi.calc(1);
+        double b3 = bmi.calc(2);
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        
+        
     }
     
 }
