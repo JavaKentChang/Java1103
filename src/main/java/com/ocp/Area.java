311948acf04bf534afa25260eae62b11353337d2
MB-study <MB-study@192.168.1.109>
2020-11-03 20:59:38 +0800

    Java-Calc2

--- src/main/java/com/ocp/Area.java
+++ src/main/java/com/ocp/Area.java
@@ -9,6 +9,9 @@ public class Area {
         int sum = w * h;
         System.out.println(sum);
     }
-        
+    int calcNotPrint(){// 代表有回傳一個int 結果
+        int sum = w * h;
+        return sum; // 代表它有回傳一個結果
+    }    
         
 }
--- src/main/java/com/ocp/Calc.java
+++ src/main/java/com/ocp/Calc.java
@@ -7,7 +7,11 @@ public class Calc {
         Area a1 = new Area();
         a1.h = 100;
         a1.w = 50;
+        //第一種無回傳值的方法;
         a1.calcAndPrint();
+        //第二種有回傳值的方法;
+        int sum2 = a1.calcNotPrint();//回傳一個int值後，接起來。
+        System.out.println(sum2);//印出那個回傳值
     }
     
 }
