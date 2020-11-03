9ac39c7e93fb67945c1cc11dd153d67999e77063
MB-study <MB-study@192.168.1.109>
2020-11-03 21:36:02 +0800

    Java-Calc3

---
+++ src/main/java/com/ocp/PrintStars.java
@@ -0,0 +1,19 @@
+
+package com.ocp;
+public class PrintStars {
+    public static void main(String[] args) {
+        int line =7;
+        /*
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*");
+        */
+        for(int x=1;x<=7;x++){
+            
+            System.out.println("*");
+        }
+    }
+    
+}
