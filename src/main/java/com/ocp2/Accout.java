77509611e872ebe90ceae35b85d3bc9d3f5fc3c1
MB-study <MB-study@192.168.1.109>
2020-11-05 20:30:34 +0800

    Java-1091105

--- src/main/java/com/ocp2/Accout.java
+++ src/main/java/com/ocp2/Accout.java
@@ -5,7 +5,8 @@ package com.ocp2;
 public class Accout {
     String name; // 戶名
     private int balance; // 餘額 // 私有的變數，只有自己看得到
-        //存款
+    
+    //存款程式
     void setBalance(int money){
         if(money >=0){
             balance += money;
@@ -15,19 +16,15 @@ public class Accout {
         }
     
     }
-    //提款
+    //提款程式
     void withdraw(int money){
         if(money >0 && balance>=money){
         balance -= money;
         System.out.printf("提款 %d 成功! \n",money);
         }else{
             System.out.printf("提款 %d 失敗(提款金額必須大於0或餘額不足!) \n",money); 
-            
            }
-    
-    }
-    
-    
+    }   
     //印出帳戶餘額
     void printBalance(){
         System.out.printf("name：%s, balance:%,d\n",name,balance);  
