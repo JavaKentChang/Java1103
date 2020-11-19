6c7a91e3ec5a2c065bb1b46159d25c815e648453
MB-study <MB-study@192.168.1.109>
2020-11-19 18:49:41 +0800

    Java-1091117-2

---
+++ src/main/java/com/ocp6/ForLoop.java
@@ -0,0 +1,15 @@
+
+package com.ocp6;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        int[] scores = {100,90,80};
+        //standard for-loop
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(scores[i]);
+        //Enhanced for-loop
+            
+        }
+    }
+    
+}
