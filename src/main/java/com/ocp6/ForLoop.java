2138d0f113648ec1a4395bd07f7f74640e5889d9
MB-study <MB-study@192.168.1.109>
2020-11-19 19:10:12 +0800

    Java-1091117-2

--- src/main/java/com/ocp6/ForLoop.java
+++ src/main/java/com/ocp6/ForLoop.java
@@ -7,9 +7,15 @@ public class ForLoop {
         //standard for-loop
         for (int i = 0; i < scores.length; i++) {
             System.out.println(scores[i]);
-        //Enhanced for-loop
-            
-        }
+            }
+        //Enhanced for-loop 
+        //for(資料型別 每個元素的名字: 要分析的資料)
+        //只能往下讀取，不能往前讀取如 i--
+        for(int s:scores) {
+            System.out.println(s);  
+            }
+  
+        
     }
     
 }
