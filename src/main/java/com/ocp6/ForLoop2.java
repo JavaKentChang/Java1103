525fb9aaf06c9e1818a0cfe4e8810153e1d1abef
MB-study <MB-study@192.168.1.109>
2020-11-19 19:17:44 +0800

    Java-1091117-2

---
+++ src/main/java/com/ocp6/ForLoop2.java
@@ -0,0 +1,22 @@
+
+package com.ocp6;
+
+public class ForLoop2 {
+    public static void main(String[] args) {
+        double [][] rows = {
+            {170.0,180.5,165.3},{60.4,89.5,41.2}             
+        };
+        for (double[] row : rows) {
+            for(double d :row){
+                System.out.println(d);
+                
+            }
+            }
+            
+        }
+        
+    }    
+        
+    }
+ 
+}
