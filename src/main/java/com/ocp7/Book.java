fffaa4ad9cd4769f5db210ed2e0f67fc2e3ceb62
MB-study <MB-study@192.168.1.109>
2020-11-24 19:09:37 +0800

    Java-1091117-2

---
+++ src/main/java/com/ocp7/Book.java
@@ -0,0 +1,43 @@
+
+package com.ocp7;
+
+public class Book { //物件屬性
+private String name;
+private int price;
+
+//預設建構子
+public Book(){
+    System.out.println("執行預設建構子");
+}
+
+//自己設定一個建構子，帶參數值的
+
+public Book(String name,int price){
+    System.out.println("執行自定建構子");
+    this.name = name;
+    this.price = price;
+}
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "name=" + name + ", price=" + price + '}';
+    }
+
+}
