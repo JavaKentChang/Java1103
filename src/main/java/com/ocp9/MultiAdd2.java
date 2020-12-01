f4e71a35ac7b179db1846028a11a1dc7ee179d93
MB-study <MB-study@192.168.1.109>
2020-12-01 21:38:51 +0800

    Java-1091117-2

--- src/main/java/com/ocp9/MultiAdd2.java
+++ src/main/java/com/ocp9/MultiAdd2.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.ocp9;
 
-/**
- *
- * @author MB-study
- */
+
 public class MultiAdd2 {
-    
+    public static void print(int x , int y){
+        System.out.println("A");
+    }
+    public static void print(int ... nums){
+        System.out.println("B");
+        //... 0~多
+    }
+    public static void main(String[] args) {
+        print(1);  // 會先滿足0~多      所以答案是B
+        print(1,2); //會滿足兩個參數的   所以答案是A
+        print(1,2,3); //會滿足0~多的部份 所以答案是B
+    }
 }
