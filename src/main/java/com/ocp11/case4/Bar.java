ba8c429f410cdb6157bf251970b35fe343554e23
MB-study <MB-study@192.168.1.109>
2020-12-10 20:03:19 +0800

    Java-1091117-2

---
+++ src/main/java/com/ocp11/case4/Bar.java
@@ -0,0 +1,22 @@
+
+package com.ocp11.case4;
+
+
+public class Bar {
+    int x = 1000;
+    public void hello(){
+        final int x = 100;//java 8 以前都需要寫final,java 8 之後就不用寫
+        //x++;
+        class Foo{
+            void printME(){
+                System.out.println("I am Foo");
+                //若要存取hello() 外部方法的區域變數
+                //則該便是必須是final
+                System.out.println(x);
+                
+            }  
+        }
+        Foo foo = new Foo();
+        foo.printME();
+    }
+}
