66ca6101128a4640e16d5d1daf0a0ed02decd991
MB-study <MB-study@DESKTOP-KVQTFGR>
2020-12-17 20:41:50 +0800

    Java

--- src/main/java/com/ocp12/lambda3/Main.java
+++ src/main/java/com/ocp12/lambda3/Main.java
@@ -1,6 +1,8 @@
 
 package com.ocp12.lambda3;
 
+import java.util.function.Predicate;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -14,6 +16,14 @@ public class Main {
         //食品檢驗
         int foodScore = 100;
         checkScore(foodScore, (x)-> x==100); 
+        
+        //使用 Predicate
+        //https://docs.oracle.com/javase/8/docs/api/java/util/function/Predicate.html
+        //boolean test(T t)
+        Predicate<Double> pass = (x)-> x==0;
+        System.out.println(pass.test(0.18));
+        Predicate<Double> bmiPass = (x)-> x>=18 && x<28;
+        System.out.println(bmiPass.test(21.76));     
     }
     
     public static void checkScore(int x, Pass p) {
--- src/main/java/com/ocp12/lambda4/Main.java
+++ src/main/java/com/ocp12/lambda4/Main.java
@@ -1,12 +1,20 @@
 
 package com.ocp12.lambda4;
 
+import java.util.function.Consumer;
+
 public class Main {
     public static void main(String[] args) {
         Print print1 =(x)->System.out.println(x);
         Print print2 = System.out::println;
         print1.display(100);
         print2.display(100);
+        
+        //使用 Consumer
+        //https://docs.oracle.com/javase/8/docs/api/java/util/function/Consumer.html
+        //void accept(T t)
+        Consumer<String> consumer = (x) -> System.out.println(x+x);
+        consumer.accept("Ha");
     }
     
 }
