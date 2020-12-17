1911ec64b20b2982c7ab11d8b6121c7f3acc7905
MB-study <MB-study@DESKTOP-KVQTFGR>
2020-12-17 19:10:53 +0800

    Java

---
+++ src/main/java/com/ocp12/lambda1/Main2.java
@@ -0,0 +1,12 @@
+
+package com.ocp12.lambda1;
+
+public class Main2 {
+    public static void main(String[] args) {
+        //Java 8 Lambda
+        Add add = (int x, int y) -> System.out.println(x+y);
+        add.sum(10, 20);
+        Add add2 = (int x, int y) -> System.err.println(x*y);
+        add2.sum(10, 20);
+    }
+}
