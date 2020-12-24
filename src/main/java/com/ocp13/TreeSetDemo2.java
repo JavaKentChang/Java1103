5d75f1e7aa944ba2c8bfaca2e8aacc8c7610b7cf
MB-study <MB-study@DESKTOP-KVQTFGR>
2020-12-24 19:37:43 +0800

    Java

---
+++ src/main/java/com/ocp13/TreeSetDemo2.java
@@ -0,0 +1,15 @@
+
+package com.ocp13;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetDemo2 {
+    public static void main(String[] args) {
+        Set<Exam> exams = new TreeSet<>();//Exam 裡沒有實作，會發生Exam cannot be cast to java.lang.Comparable
+        for (int i = 0; i < 5; i++) {
+            exams.add(new Exam());            
+        }
+        System.out.println(exams);
+    }
+}
