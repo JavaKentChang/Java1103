eb7d9a8dde313fd168f851484035c8cc6dfe55e5
MB-study <MB-study@DESKTOP-KVQTFGR>
2021-01-07 18:54:30 +0800

    Java

--- src/main/java/com/ocp16_exception/ExceptionDemo1.java
+++ src/main/java/com/ocp16_exception/ExceptionDemo1.java
@@ -1,9 +1,9 @@
-package com.ocp16;
+package com.ocp16_exception;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class ExceptionDemo2 {
+public class ExceptionDemo1 {
     public static void main(String[] args) {
         play();
     }
@@ -13,12 +13,6 @@ public class ExceptionDemo2 {
         System.out.print("假設分子(x)是10, 請輸入分母(y): ");
         try {
             int y = sc.nextInt();//可能會發生InputMismatchException的例外
-            //runtimeException 先用程式碼解決
-            //先判斷y 是否= 0
-            if (y==0) {
-               System.out.println("分母不可為0");
-                play();
-            }
             int result = x/y;
              System.out.println(result);
         } catch (InputMismatchException e) {
@@ -26,8 +20,11 @@ public class ExceptionDemo2 {
             System.out.println("錯誤類型: "+ e);
             System.out.println("錯誤原因: "+ e.getMessage());
             play();
-        } finally{
-            System.out.println("遊戲結束");
+        }catch (ArithmeticException ae) {
+            System.out.println("分母不可為0");
+            System.out.println("錯誤類型: "+ ae);
+            System.out.println("錯誤原因: "+ ae.getMessage());
+            play();
         }
         
         
