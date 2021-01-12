058412a9ce08c5f74a1365a6b93516055715730e
MB-study <MB-study@DESKTOP-KVQTFGR>
2021-01-12 21:45:21 +0800

    Java

---
+++ src/main/java/com/ocp18_thread/RunnerMain.java
@@ -0,0 +1,19 @@
+
+package com.ocp18_thread;
+
+
+public class RunnerMain {
+    public static void main(String[] args) {
+        Runner r1 = new Runner();
+        Runner r2 = new Runner();
+        //分時多工
+        r1.setName("烏龜");
+        r2.setName("兔子");
+        //讓烏龜權證變高
+        r1.setPriority(Thread.MAX_PRIORITY);//1~10
+        r2.setPriority(Thread.MIN_PRIORITY);//1~10
+        
+        r1.start();//啟動執行緒
+        r2.start();
+    }
+}
